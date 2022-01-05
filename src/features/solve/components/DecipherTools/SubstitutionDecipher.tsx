7d15992cae03f5793b1bb04db3fd7f51b3d20389
Marc Widmer <mwidmer@protonmail.com>
2022-01-05 20:19:26 +0100

    wip substitution decipher component

--- src/features/solve/components/DecipherTools/SubstitutionDecipher.tsx
+++ src/features/solve/components/DecipherTools/SubstitutionDecipher.tsx
@@ -1,4 +1,16 @@
-import { HStack, VStack, Text, Grid, Input, InputLeftAddon, InputGroup } from '@chakra-ui/react';
+import {
+  HStack,
+  VStack,
+  Text,
+  Grid,
+  Input,
+  InputLeftAddon,
+  InputGroup,
+  GridItem,
+  FormControl,
+  Button,
+  FormErrorMessage,
+} from '@chakra-ui/react';
 import { useState } from 'react';
 import { BasicBox } from '../../../../components/BasicBox';
 import { DecipherProps } from './Decipher.ds';
@@ -6,32 +18,53 @@ import { DecipherProps } from './Decipher.ds';
 export const SubstitutionDecipher: React.FC<DecipherProps> = ({ text, setDecipheredText }) => {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
   const [substitutionKey, setSubstitutionKey] = useState(alphabet);
+  const [isFieldInvalid, setIsFieldInvalid] = useState(Array(26).fill(false) as boolean[]);
+  const [isInvalid, setIsInvalid] = useState(false);
   return (
     <BasicBox>
       <VStack>
         <Text>Substitution Decipher</Text>
-        <Grid templateColumns='repeat(4,1fr)' gap='4'>
+        <Grid autoFlow='column' templateRows='repeat(7, 1fr)' gap='4'>
           {alphabet.map((v, i) => (
-            <InputGroup>
-              <InputLeftAddon width='3rem' justifyContent='center'>
-                {v}
-              </InputLeftAddon>
-              <Input
-                width='3rem'
-                textAlign='center'
-                defaultValue={v}
-                onChange={(e) =>
-                  setSubstitutionKey(
-                    (() => {
-                      substitutionKey[i] = e.target.value;
-                      return substitutionKey;
-                    })()
-                  )
-                }
-              />
-            </InputGroup>
+            <GridItem key={i}>
+              <FormControl isInvalid={isFieldInvalid[i] || isInvalid}>
+                <InputGroup>
+                  <InputLeftAddon width='3rem' justifyContent='center'>
+                    {v}
+                  </InputLeftAddon>
+                  <Input
+                    width='3rem'
+                    textAlign='center'
+                    defaultValue={v}
+                    onChange={(e) => {
+                      e.preventDefault();
+                      const val = e.target.value.toUpperCase();
+                      e.target.value = val;
+                      const match = val.match('[a-zA-Z]');
+                      isFieldInvalid[i] = !match || match.length != 1;
+                      setIsFieldInvalid([...isFieldInvalid]);
+                      if (isFieldInvalid[i]) {
+                        setSubstitutionKey(
+                          (() => {
+                            substitutionKey[i] = val.toUpperCase();
+                            return substitutionKey;
+                          })()
+                        );
+                      }
+                    }}
+                  />
+                </InputGroup>
+              </FormControl>
+            </GridItem>
           ))}
         </Grid>
+        {isInvalid && (
+          <FormErrorMessage>
+            The entered key isn't a permutation of the alphabet. Make sure you don't have any
+            duplicates.{' '}
+          </FormErrorMessage>
+        )}
+        <Button onClick={() => setIsInvalid(!isInvalid)}>Toggle Invalid</Button>
       </VStack>
     </BasicBox>
   );
