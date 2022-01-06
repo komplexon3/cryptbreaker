8729c205cd3009c8fe32bc806b76ccc434ba5381
Marc Widmer <mwidmer@protonmail.com>
2022-01-06 15:51:58 +0100

    cleanup vignere tests

--- src/features/solve/util/vignere.test.ts
+++ src/features/solve/util/vignere.test.ts
@@ -1,3 +1,4 @@
+import { verifyVignereKey } from '.';
 import { vignereDecrypt, vignereEncrypt } from './vignere';
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
@@ -20,14 +21,15 @@ const getPermutation = (s: string): string => {
 const getRandomVignereKey = (len: number, alphabet: string) =>
   [...Array(len).fill('')].map((_) => alphabet[getRandomInt(0, alphabet.length - 1)]).join('');
 
+const standardPlainText =
+  'hello, my name is marc widmer and I am testing the funtionality of this component.';
+
 describe('vignereEncrypt, vignereDecrypt', () => {
   test('basic test encrypt', () => {
     const vignereKey = 'ako';
-    const plainText =
-      'hello, my name is marc widmer and I am testing the funtionality of this component.';
     const expectedCipherText =
       'hozly, ay xomo ws worm kinaeb onn W aw hechixu trs febtscnkzidm op hhsg cyapybexh.';
-    const cipherText = vignereEncrypt(plainText, vignereKey);
+    const cipherText = vignereEncrypt(standardPlainText, vignereKey);
     expect(cipherText).toMatch(expectedCipherText);
   });
 
@@ -35,19 +37,15 @@ describe('vignereEncrypt, vignereDecrypt', () => {
     const vignereKey = 'ako';
     const cipherText =
       'hozly, ay xomo ws worm kinaeb onn W aw hechixu trs febtscnkzidm op hhsg cyapybexh.';
-    const expectedPlainText =
-      'hello, my name is marc widmer and I am testing the funtionality of this component.';
     const plainText = vignereDecrypt(cipherText, vignereKey);
-    expect(plainText).toMatch(expectedPlainText);
+    expect(plainText).toMatch(standardPlainText);
   });
 
   test('basic test encrypt - one character key', () => {
     const vignereKey = 'h';
-    const plainText =
-      'hello, my name is marc widmer and I am testing the funtionality of this component.';
     const expectedCipherText =
       'olssv, tf uhtl pz thyj dpktly huk P ht alzapun aol mbuapvuhspaf vm aopz jvtwvulua.';
-    const cipherText = vignereEncrypt(plainText, vignereKey);
+    const cipherText = vignereEncrypt(standardPlainText, vignereKey);
     expect(cipherText).toMatch(expectedCipherText);
   });
 
@@ -55,10 +53,8 @@ describe('vignereEncrypt, vignereDecrypt', () => {
     const vignereKey = 'h';
     const cipherText =
       'olssv, tf uhtl pz thyj dpktly huk P ht alzapun aol mbuapvuhspaf vm aopz jvtwvulua.';
-    const expectedPlainText =
-      'hello, my name is marc widmer and I am testing the funtionality of this component.';
     const plainText = vignereDecrypt(cipherText, vignereKey);
-    expect(plainText).toMatch(expectedPlainText);
+    expect(plainText).toMatch(standardPlainText);
   });
 
   test('arbitrary strings, arbitrary keys - encrypt and decrypt', () => {
@@ -74,7 +70,7 @@ describe('vignereEncrypt, vignereDecrypt', () => {
     }
   });
 
-  test('encrypt - upper case vignere key', () => {
+  test('encrypt/decrypt - upper case vignere key', () => {
     for (let i = 0; i < 10; i++) {
       const testPlainText = getRandomString(getRandomInt(20, 200));
       const vignereKey = getRandomVignereKey(getRandomInt(1, 50), alphabet.toUpperCase());
@@ -84,7 +80,7 @@ describe('vignereEncrypt, vignereDecrypt', () => {
     }
   });
 
-  test('encrypt - lower case vignere key', () => {
+  test('encrypt/decrypt - lower case vignere key', () => {
     for (let i = 0; i < 10; i++) {
       const testPlainText = getRandomString(getRandomInt(20, 200));
       const vignereKey = getRandomVignereKey(getRandomInt(1, 50), alphabet);
@@ -95,25 +91,23 @@ describe('vignereEncrypt, vignereDecrypt', () => {
   });
 
   test('encrypt - invalid vignere key', () => {
-    const plainText =
-      'hello, my name is marc widmer and I am testing the funtionality of this component.';
-    expect(() => vignereEncrypt(plainText, '')).toThrowError('vignere key invalid');
-    expect(() => vignereEncrypt(plainText, '1231819')).toThrowError('vignere key invalid');
-    expect(() => vignereEncrypt(plainText, 'hello this text contains spaces and :!$')).toThrowError(
+    expect(() => vignereEncrypt(standardPlainText, '')).toThrowError('vignere key invalid');
+    expect(() => vignereEncrypt(standardPlainText, '1231819')).toThrowError('vignere key invalid');
+    expect(() =>
+      vignereEncrypt(standardPlainText, 'hello this text contains spaces and :!$')
+    ).toThrowError('vignere key invalid');
+    expect(() => vignereEncrypt(standardPlainText, 'a89q3(8opu:{22jl')).toThrowError(
       'vignere key invalid'
     );
-    expect(() => vignereEncrypt(plainText, 'a89q3(8opu:{22jl')).toThrowError('vignere key invalid');
   });
 
   test('decrypt - invalid vignere key', () => {
-    const cipherText =
-      'hello, my name is marc widmer and I am testing the funtionality of this component.';
-    expect(() => vignereDecrypt(cipherText, '')).toThrowError('vignere key invalid');
-    expect(() => vignereDecrypt(cipherText, '1231819')).toThrowError('vignere key invalid');
+    expect(() => vignereDecrypt(standardPlainText, '')).toThrowError('vignere key invalid');
+    expect(() => vignereDecrypt(standardPlainText, '1231819')).toThrowError('vignere key invalid');
     expect(() =>
-      vignereDecrypt(cipherText, 'hello this text contains spaces and :!$')
+      vignereDecrypt(standardPlainText, 'hello this text contains spaces and :!$')
     ).toThrowError('vignere key invalid');
-    expect(() => vignereDecrypt(cipherText, 'a89q3(8opu:{22jl')).toThrowError(
+    expect(() => vignereDecrypt(standardPlainText, 'a89q3(8opu:{22jl')).toThrowError(
       'vignere key invalid'
     );
   });
