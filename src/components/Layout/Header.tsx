18775eb418de57f8f22111052d790a512b9a2d2d
Marc Widmer <mwidmer@protonmail.com>
2022-01-17 13:39:14 +0100

    make header link to home

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,11 +1,14 @@
 import { Center, Heading } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <Center>
-      <Heading as='h1' size='4xl' marginY='10'>
-        Crypt Breaker
-      </Heading>
+      <Link to='/'>
+        <Heading as='h1' size='4xl' marginY='10'>
+          Crypt Breaker
+        </Heading>
+      </Link>
     </Center>
   );
 };
