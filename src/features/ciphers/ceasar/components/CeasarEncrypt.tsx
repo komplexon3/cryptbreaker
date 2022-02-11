206bbf0709496e2a5d97cbae4f9c8f5ca6267256
Marc Widmer <mwidmer@protonmail.com>
2022-02-11 10:43:35 +0100

    change ceasar input props, set text on render, closes #11

--- src/features/ciphers/ceasar/components/CeasarEncrypt.tsx
+++ src/features/ciphers/ceasar/components/CeasarEncrypt.tsx
@@ -1,12 +1,19 @@
 import { Card } from '@/components';
 import { EncryptProps } from '@/types';
+import { useEffect, useState } from 'react';
 import { ceasarEncrypt } from '../utils/ceasar';
 import { CeasarKeyInput } from './CeasarKeyInput';
 
 export const CeasarEncrypt: React.FC<EncryptProps> = ({ text, setCipherText, onClose }) => {
+  const [key, setKey] = useState(3);
+
+  useEffect(() => {
+    setCipherText(ceasarEncrypt(text, key));
+  }, [key, text, setCipherText]);
+
   return (
     <Card title='Encrypt with ceasar' onClose={onClose}>
-      <CeasarKeyInput onChange={(v) => setCipherText(ceasarEncrypt(text, v))} />
+      <CeasarKeyInput minValue={0} maxValue={25} value={key} onChange={(v) => setKey(v)} />
     </Card>
   );
 };
